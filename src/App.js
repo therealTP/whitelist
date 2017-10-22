0725d1267b7ecd602cbfc69e5230201ce0faa74d
therealtp <mr.tylerpalmer@gmail.com>
2017-10-22 00:26:05 -0700

    successful hit of backend & surfacing returned data to frontend

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const myHeaders = new Headers();
 
 const myInit = { method: 'GET',
                headers: myHeaders,
-               mode: 'no-cors',
+               mode: 'cors',
                cache: 'no-cache'};
 
 class App extends Component {
@@ -25,11 +25,15 @@ class App extends Component {
     fetch(
       'http://localhost:9000/source?limit=10', myInit
     ).then(res => {
-      console.log("WOOPS", res);
+      if (!res.ok) {
+        throw Error;
+      }
       return res.json();
     }).then(data => {
       console.log("DATA", data);
       this.setState({sources: data.response.results});
+    }).catch(err => {
+      console.log("ERR", err);
     });
   }
 
@@ -49,7 +53,7 @@ class App extends Component {
         </ul>
         <ul>
           {this.state.sources.map(source =>
-            <li key={source.id}>{source.name}</li>)}
+            <li key={source.id}>{source.name} @ <a href={source.websiteUrl} target="_blank">{source.websiteUrl}</a></li>)}
         </ul>
       </div>
     );
